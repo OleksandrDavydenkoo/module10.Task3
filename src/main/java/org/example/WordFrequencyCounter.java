9ec9368f2c38da367bf5a2a3f4a4df69bee3a98e
Oleksandr <ol.davydenkoo@gmail.com>
2024-02-11 15:18:31 +0100

    Module10.Task3

--- src/main/java/org/example/WordFrequencyCounter.java
+++ src/main/java/org/example/WordFrequencyCounter.java
@@ -3,7 +3,6 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class WordFrequencyCounter {
